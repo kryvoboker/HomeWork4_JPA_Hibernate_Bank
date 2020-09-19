22c67ee8b35b7bea9bfb761d5a26ac2d0605fb68
kryvoboker <kamaz_cs@live.ru>
2020-09-19 17:58:44 +0300

    Пытался вытащить переменные с JSON

---
+++ src/main/java/ru/live/kamaz_cs/PrivatbankJson.java
@@ -0,0 +1,51 @@
+package ru.live.kamaz_cs;
+
+public class PrivatbankJson {
+    //    [{"ccy":"USD","base_ccy":"UAH","buy":"27.55000","sale":"27.95000"}]
+    public String ccy;
+    public String base_ccy;
+    public String buy;
+    public String sale;
+
+    public String getCcy() {
+        return ccy;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
+    public String getBase_ccy() {
+        return base_ccy;
+    }
+
+    public void setBase_ccy(String base_ccy) {
+        this.base_ccy = base_ccy;
+    }
+
+    public String getBuy() {
+        return buy;
+    }
+
+    public void setBuy(String buy) {
+        this.buy = buy;
+    }
+
+    public String getSale() {
+        return sale;
+    }
+
+    public void setSale(String sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public String toString() {
+        return "PrivatbankJson{" +
+                "ccy='" + ccy + '\'' +
+                ", base_ccy='" + base_ccy + '\'' +
+                ", buy='" + buy + '\'' +
+                ", sale='" + sale + '\'' +
+                '}';
+    }
+}
